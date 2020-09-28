28fa9e06734dbaa866121380ea6b5bdab487a528
jefer <jefer@pop-os.localdomain>
2020-09-28 16:38:56 -0300

    up challenges node

--- 06_NodeJS/challenges/count.js
+++ 06_NodeJS/challenges/count.js
@@ -1,11 +1,11 @@
 // TODO função que recebe a, b e retorne a soma deles; 
 function sum(a, b) {
-  return 0;
+    return a + b;
 }
 
 // TODO função que recebe a, b e retorne a subitração deles; 
 function subtraction(a, b) {
-  return 0;
+    return a - b;
 }
 
 module.exports = { sum, subtraction };
