8d5e2fcc8b829417b2b8c3c7b4a4493a7aafd6a0
Gabriel Prando <gprando55@gmail.com>
2020-10-15 21:46:25 -0300

    Update routes.js

--- Jeferson/09_API_Mongo_Express/src/routes.js
+++ Jeferson/09_API_Mongo_Express/src/routes.js
@@ -9,8 +9,8 @@ const userController = new UserController();
 
 routes.post('/users', userController.create);
 routes.get('/users', userController.list);
-routes.get('/users/:cpf', userController.show);
-routes.put('/users/:cpf', userController.update);
-routes.delete('/users/:cpf', userController.delete);
+routes.get('/users/:id', userController.show);
+routes.put('/users/:id', userController.update);
+routes.delete('/users/:id', userController.delete);
 
 module.exports = routes;
